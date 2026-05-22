efe858dc66c6a6aa5e79515202de57de30ce1dad
quii <chris.james@springernature.com>
2026-05-22 11:52:56 +0100

    Fix gitignore: exclude target/ build output

--- hello-world/target/debug/deps/hello_world-f63fdf2eb599663c.d
+++
@@ -1,5 +0,0 @@
-/Users/cju3744/dev/learn-rust-with-tests/hello-world/target/debug/deps/hello_world-f63fdf2eb599663c.d: src/main.rs
-
-/Users/cju3744/dev/learn-rust-with-tests/hello-world/target/debug/deps/hello_world-f63fdf2eb599663c: src/main.rs
-
-src/main.rs:
--- hello-world/target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-/Users/cju3744/dev/learn-rust-with-tests/hello-world/target/debug/hello-world: /Users/cju3744/dev/learn-rust-with-tests/hello-world/src/main.rs
