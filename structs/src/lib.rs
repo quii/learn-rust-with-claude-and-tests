930154281ee4714ac329da19ae80d9ac5f22af37
quii <chris.james@springernature.com>
2026-05-22 16:29:43 +0100

    Structs: add area() method

--- structs/src/lib.rs
+++ structs/src/lib.rs
@@ -7,6 +7,10 @@ impl Rectangle {
     pub fn perimeter(&self) -> f64 {
         2.0 * (self.width + self.height)
     }
+
+    pub fn area(&self) -> f64 {
+        self.width * self.height
+    }
 }
 
 #[cfg(test)]
@@ -17,5 +21,11 @@ mod tests {
     fn perimeter_of_rectangle() {
         let r = Rectangle { width: 10.0, height: 5.0 };
         assert_eq!(r.perimeter(), 30.0);
+    }
+
+    #[test]
+    fn area_of_rectangle() {
+        let r = Rectangle { width: 10.0, height: 5.0 };
+        assert_eq!(r.area(), 50.0);
     }
 }
